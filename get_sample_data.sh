45465635cb6f0938d6379f5fe44e94c2eb3eaf8c
Adrian Torrie <adriantorrie@gmail.com>
2017-05-24 20:37:59 +1000

    fixing get_sample_data v2

--- get_sample_data.sh
+++ get_sample_data.sh
@@ -4,7 +4,7 @@ mkdir -p ./data/raw/sample/IMG
 
 wget https://d17h27t6h515a5.cloudfront.net/topher/2016/December/584f6edd_data/data.zip -O ./data/raw/sample/data.zip
 
-unzip -o ./raw/sample/data.zip
+unzip -o ./data/raw/sample/data.zip
 
 mv ./data/raw/sample/data/IMG ./data/raw/sample/IMG
 mv ./data/raw/sample/data/driving_log.csv ./data/raw/sample/driving_log.csv
